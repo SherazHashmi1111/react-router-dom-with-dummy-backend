b1e2d1e58d913863747fa8b49030c956e7de46a4
SherazHashmi1111 <sherazhashmi111@gmail.com>
2024-03-23 12:30:48 +0500

    new event added in backend

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,8 +1,30 @@
 import React from "react";
-import EventForm from '../components/EventForm'
+import EventForm from "../components/EventForm";
+import { redirect } from "react-router-dom";
 
 function NewEvent() {
-  return <EventForm/>
+  return <EventForm />;
 }
 
 export default NewEvent;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+  const eventData = {
+    title: data.get("title"),
+    date: data.get("date"),
+    image: data.get("image"),
+    description: data.get("description"),
+  };
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+  if(!response.ok){
+    return <p>Error adding new event</p>
+  }
+  return redirect("/events");
+}
